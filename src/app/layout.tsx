570c3fc511c1055eb435e4f7972c8089f1ff0360
HytoMobi <vikash@hytomobi.in>
2024-09-26 18:28:51 +0530

    Add Google Analytics tool

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/toaster";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // Import the Script component for adding analytics
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* Add Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-EDD7G93BTD`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-EDD7G93BTD', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+        
         {children}
         <Toaster />
       </body>
